b308804970d4797a71416dc61f91554bf4d2f9b3
Abel <abel291m@gmail.com>
2022-06-09 07:18:24 -0500

    capture img

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import DetailsPokemon from './Pages/DetailsPokemon';
 const App = () => {
 	return (
-		<div className="container mx-auto">
+		<div className="container mx-auto max-w-7xl">
 			<div className="text-center py-10">
 				<h1 className="text-5xl font-bold">Poke Api</h1>
 			</div >
